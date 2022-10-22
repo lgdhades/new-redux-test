c4d15351780148a0b39c6af2055b51adf6a8ea3f
lgdhades <hades987456@outlook.com>
2022-10-22 21:11:16 +0200

    feat : finish the first 4 points

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
 import './App.css'
+import { configureStore, createAction } from '@reduxjs/toolkit'
+import { Action, combineReducers, Dispatch } from 'redux'
+import { createRoot } from 'react-dom/client'
+import { Provider, useDispatch, useSelector } from 'react-redux'
 
-function App() {
-  return <div>Hello App</div>
+// A simple counter that keep incrementing automatically by 1 every second ,
+// and has 2 buttons to manually increment or decrement the counter.
+// if you used the buttons to increment or decerment the counter manaually,
+// the counter will keep incrementing automatically by 1 after you finish using the buttons
+// and it will start from the last number the counter has now.
+
+const increment = createAction('increment')
+const decrement = createAction('decrement')
+
+// Increment the counter by 1 if increment action dispatched,
+// decrement the counter by 1 if decrement action dispatched,
+// otherwise leave the counter number as is.
+const counter = (startNumber = 0, action: Action) =>
+  increment.match(action)
+    ? startNumber + 1
+    : decrement.match(action)
+    ? startNumber - 1
+    : startNumber
+
+const root = combineReducers({ counter })
+
+// Increments the counter automatically by 1 every second
+const timer = ({ dispatch }: { dispatch: Dispatch<Action> }) => {
+  setInterval(() => dispatch(increment()), 1000)
+
+  return (next: (arg0: Action) => void) => (action: Action) => {
+    next(action)
+  }
+}
+
+export const ReduxStore = configureStore({
+  reducer: root,
+  middleware: [timer],
+})
+
+const App = () => {
+  const dispatch = useDispatch()
+  const counter = useSelector(
+    (state: ReturnType<typeof ReduxStore.getState>) => state.counter
+  )
+
+  return (
+    <>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {counter}
+      <button onClick={() => dispatch(increment())}>+</button>
+    </>
+  )
 }
 
 export default App
---
+++ src/Instructions.md
@@ -0,0 +1,19 @@
+/*
+This is a React app that uses Redux store with a reducer and a middleware.
+
+Your assignment is to make it more readable, and also add some functionality.
+
+Specifically, please consider doing the following, in no particular order:
+ 1) Replace single-letter variable names with something more descriptive. >>>>> DONE
+ 2) Provide comments for each of the functions. >>>> DONE
+ 3) Provide a general comment for the entire program, describing its purpose. >>>>> DONE
+ 4) Replace type any with something more specific >>>> DONE
+ 5) Do not allow timer to increment counter past 10.
+ 6) Trigger an alert when counter reaches 20.
+ 7) If counter goes below zero, make timer decrement rather than increment it.
+ 8) Add a drop-down to select increment and decrement step between 1, 2, and 3.
+ 9) Use monospace font for all text on the page.
+ 10) Create unit tests for increment, decrement and counter functions.
+ 11) Create package configuration that could be used to serve the page.
+ 12) Change App to log the timestamp of when that component did mount.
+*/
